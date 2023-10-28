61c02ffd2c8dc10327efd49241ded3a30e2cc2ca
juerson <zhangrson@163.com>
2023-10-28 20:30:46 +0800

    update

--- src/2、不需要外部配置文件版本1，由用户输入地址和端口.rs
+++ src/2、不需要外部配置文件版本1，由用户输入地址和端口.rs
@@ -1,72 +1,110 @@
 use std::io;
 use std::io::Write;
+use regex::Regex;
+use std::net::{Ipv4Addr, Ipv6Addr};
 
 fn main() {
 	println!("{}", "*".repeat(100));
-	println!("CF VLESS链接中的端口，可以根据下面的TLS加密情况设置：");
+	println!("CF vless链接中的端口，可以根据下面所示的情况来设置：");
 	println!("非TLS加密：80 8080 8880 2052 2082 2086 2095");
 	println!("是TLS加密：443 2053 2083 2087 2096 8443");
     println!("{}", "+".repeat(100));
-
-	let mut link:String;
-    loop {
-		link = String::new();
-        print!("输入VLESS LINK："); // 输入一个可用的vless节点
-		io::stdout().flush().unwrap();
-        std::io::stdin().read_line(&mut link).expect("读取输入失败");
-
-        if link.starts_with("vless") && link.len() > 100 {
-            break;
-        }
-    }
+	
+	// 获取用户输入的link链接
+	let link = get_linke_from_user_input();
 
     println!("{}", "+".repeat(100));
 
     let before_string = link.chars().take(link.find('@').unwrap_or(link.len()) + 1).collect::<String>();
-    let parameter_string = link
-							.split('?')
-							.nth(1)
-							.and_then(|substring| substring.split('#').next())
-							.unwrap_or("");
+    let parameter_string = link.split('?').nth(1).and_then(|substring| substring.split('#').next()).unwrap_or("");
 
     loop {
-        let mut addr = String::new();
-        print!("输入优选的CDN地址(Address)：");
-		io::stdout().flush().unwrap();
-        std::io::stdin().read_line(&mut addr).expect("读取输入失败");
-        let addr = addr.trim();
-
-        if addr.is_empty() {
-            continue;
-        }
-
-        let mut port = String::new();
-		loop {
-			print!("输入CDN端口(Port)：");
-			io::stdout().flush().unwrap();
-			std::io::stdin().read_line(&mut port).expect("读取输入失败");
-			port = port.trim().to_string(); // 修剪端口字符串
-
-			if port.chars().all(char::is_numeric) && port.trim().len() >= 2 {
-				break; // 如果端口是两位以上的数字，跳出循环
-			} else if port.is_empty() && parameter_string.contains("security=tls") {
-				port = "2053".to_string();
-				break;
-			} else if port.is_empty() && !parameter_string.contains("security=tls") {
-				port = "2052".to_string();
-				break;
-			} else {
-				port.clear(); // 清空端口变量，以便重新输入
-			}
-		}
+        // 获取用户输入的地址（IP、域名）
+		let addr = get_host_from_user_input();
+		// 获取用户输入的端口
+        let port = get_port_form_user_input(parameter_string);
 
         let address = format!("{}:{}?", addr, port);
         let remarks = format!("#{}:{}", addr, port);
         let new_link = format!("{}{}{}{}", before_string, address, parameter_string, remarks);
 		
-		println!("复制下面的链接使用即可");
+		println!("生成的vless链接：");
         println!("{}", "-".repeat(100));
         println!("{}", new_link);
         println!("{}", "-".repeat(100));
+    }
+}
+
+fn get_linke_from_user_input() -> String {
+    loop {
+		let mut link = String::new();
+        print!("输入vless链接："); // 输入一个可用的vless节点
+		io::stdout().flush().unwrap();
+		link.clear(); // 清空字符串变量的内容
+        std::io::stdin().read_line(&mut link).expect("读取输入失败");
+		link = link.trim().to_string();
+
+        if link.starts_with("vless") && link.len() > 100 {
+            return link
+        }
+    }
+}
+
+fn get_host_from_user_input() -> String {
+    loop {
+        let mut addr = String::new();
+        print!("输入IP或域名：");
+        io::stdout().flush().unwrap();
+		addr.clear(); // 清空字符串变量的内容
+        io::stdin().read_line(&mut addr).expect("读取输入失败");
+		// 清除字符串左右的空白
+        addr = addr.trim().to_string();
+        if is_valid_domain(&addr) {
+			return addr.to_string()
+        } else if is_valid_ipv4(&addr) {
+            return addr.to_string();
+        } else if is_valid_ipv6(&addr) {
+            return format!("[{}]",addr.to_string())
+        }
+    }
+}
+
+fn get_port_form_user_input(parameter_string:&str) -> String {
+	loop {
+		let mut port = String::new();
+		print!("输入端口(PORT)：");
+		io::stdout().flush().unwrap();
+		port.clear(); // 清空字符串变量的内容
+		std::io::stdin().read_line(&mut port).expect("读取输入失败");
+		// 清除字符串左右的空白
+		port = port.trim().to_string();
+
+		if port.chars().all(char::is_numeric) && port.len() >= 2 && port.len() <= 5{
+			return port.to_string()
+		} else if port.is_empty() && parameter_string.contains("security=tls") {
+			return "443".to_string()
+		} else if port.is_empty() && !parameter_string.contains("security=tls") {
+			return "8080".to_string()
+		}
+	}
+}
+
+
+fn is_valid_domain(domain: &str) -> bool {
+    let re = Regex::new(r"^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$").unwrap();
+    re.is_match(domain)
+}
+
+fn is_valid_ipv4(ip: &str) -> bool {
+    match ip.parse::<Ipv4Addr>() {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+
+fn is_valid_ipv6(ip: &str) -> bool {
+    match ip.parse::<Ipv6Addr>() {
+        Ok(_) => true,
+        Err(_) => false,
     }
 }
